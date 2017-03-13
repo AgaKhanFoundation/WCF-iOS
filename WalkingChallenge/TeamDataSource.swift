8a77fe518c7932a2410beed1a22ae11887d814fc
Sami Suteria <sami.suteria@gmail.com>
2017-03-13 00:24:51 -0700

    add team vc with data source and configurable cells

---
+++ WalkingChallenge/TeamDataSource.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+class TeamDataSource: TableDataSource {
+    
+    var cells = [CellInfo]()
+    
+    func reload(completion: @escaping SuccessBlock) {
+        // TODO: Fetch team from backend
+        
+        if cells.isEmpty {
+            cells = [
+                TeamNameCellInfo(name: "Team Name"),
+                TeamMemberCellInfo(name: "Sarah Jane"),
+                TeamMemberCellInfo(name: "Mark Stevens"),
+                TeamMemberCellInfo(name: "Ali Noorali")
+            ]
+        }
+        
+        onMain {
+            completion(true)
+        }
+    }
+    
+    func addTeamMember(completion: @escaping SuccessBlock) {
+        // TODO: Push data to backend and refresh cells
+        
+        cells.append(TeamMemberCellInfo(name: "Someone New"))
+        
+        onMain {
+            completion(true)
+        }
+    }
+}
