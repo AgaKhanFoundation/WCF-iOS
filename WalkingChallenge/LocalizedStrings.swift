bec50c948ae1addb18043548040698b9d9f9b7e4
Sami Suteria <sami.suteria@gmail.com>
2017-03-12 22:49:57 -0700

    add localized strings (for future localization)

---
+++ WalkingChallenge/LocalizedStrings.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+struct Strings {
+    
+    struct NavBarTitles {
+        static let profile = NSLocalizedString("Profile", comment: "Navigation bar title for profile tab")
+        static let team = NSLocalizedString("Team", comment: "Navigation bar title for team tab")
+        static let sponsor = NSLocalizedString("Sponsor", comment: "Navigation bar title for sponsor tab")
+    }
+}
