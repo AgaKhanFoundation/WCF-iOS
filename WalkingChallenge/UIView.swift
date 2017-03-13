64b1630cafacfdd971258b95e6b6ea27f020e52a
Sami Suteria <sami.suteria@gmail.com>
2017-03-12 23:39:21 -0700

    change extension to not need param

--- WalkingChallenge/UIView.swift
+++ WalkingChallenge/UIView.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 extension UIView {
-    func addSubviews(views: [UIView]) {
+    func addSubviews(_ views: [UIView]) {
         for view in views {
             addSubview(view)
         }
