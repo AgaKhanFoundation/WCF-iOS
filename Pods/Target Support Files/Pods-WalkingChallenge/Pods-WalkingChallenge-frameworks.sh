9ff7e5509af5379cde4fe6a9d3a284465cbc8d8c
Sami Suteria <sami.suteria@gmail.com>
2017-03-12 21:27:02 -0700

    pod install

--- Pods/Target Support Files/Pods-WalkingChallenge/Pods-WalkingChallenge-frameworks.sh
+++ Pods/Target Support Files/Pods-WalkingChallenge/Pods-WalkingChallenge-frameworks.sh
@@ -87,6 +87,17 @@ strip_invalid_archs() {
   fi
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/Bolts/Bolts.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/FBSDKCoreKit/FBSDKCoreKit.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/FBSDKLoginKit/FBSDKLoginKit.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/Bolts/Bolts.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/FBSDKCoreKit/FBSDKCoreKit.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/FBSDKLoginKit/FBSDKLoginKit.framework"
+fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
 fi
