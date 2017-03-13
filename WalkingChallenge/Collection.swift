1c44892c77027812ea7f850c71db8a25ce341a33
Sami Suteria <sami.suteria@gmail.com>
2017-03-13 00:24:13 -0700

    add safe index search to collection extension

---
+++ WalkingChallenge/Collection.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+//http://stackoverflow.com/questions/25329186/safe-bounds-checked-array-lookup-in-swift-through-optional-bindings
+extension Collection where Indices.Iterator.Element == Index {
+    
+    /// Returns the element at the specified index iff it is within bounds, otherwise nil.
+    subscript (safe index: Index) -> Generator.Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
+}
