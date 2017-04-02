576389d168c16e059de719da84617002fe72704d
Saleem Abdulrasool <compnerd@compnerd.org>
2017-04-01 19:32:13 -0700

    Style: populate AKDN style guidelines

--- WalkingChallenge/StyleGuide.swift
+++ WalkingChallenge/StyleGuide.swift
@@ -1,12 +1,19 @@
 
 import UIKit
 
-// TODO: Update this based on style guide from UX/UI person
 struct Style {
   struct Colors {
-    static let basic = #colorLiteral(red: 0.1529411765, green: 0.1215686275, blue: 0.1215686275, alpha: 1) // #271F1F
-    static let white = #colorLiteral(red: 0.9882352941, green: 1, blue: 0.9607843137, alpha: 1) // #FCFFF5
-    static let accent = #colorLiteral(red: 0.344225198, green: 0.6224462986, blue: 0.1597332358, alpha: 1) // #699D3D //TODO: Replace with AKDN branding
+    // Primary
+    static let green = #colorLiteral(red: 0.0000000000, green: 0.6941176470, blue: 0.2509803921, alpha: 1)      // #00b140
+    static let black = #colorLiteral(red: 0.0000000000, green: 0.0000000000, blue: 0.0000000000, alpha: 1)      // #000000
+    static let white = #colorLiteral(red: 1.0000000000, green: 1.0000000000, blue: 1.0000000000, alpha: 1)      // #ffffff
+    static let grey = #colorLiteral(red: 0.3450980392, green: 0.3450980392, blue: 0.3568627450, alpha: 1)       // #58585b
+
+    // Secondary
+    static let gold = #colorLiteral(red: 0.7333333333, green: 0.6196078431, blue: 0.1843137254, alpha: 1)       // #bb9e2f
+    static let yellow = #colorLiteral(red: 1.0000000000, green: 0.7843137254, blue: 0.2705882352, alpha: 1)     // #ffc845
+    static let lightGreen = #colorLiteral(red: 0.5843137254, green: 0.7568627450, blue: 0.1215686274, alpha: 1) // #95c11f
+    static let darkGreen = #colorLiteral(red: 0.0156862745, green: 0.4156862745, blue: 0.2196078431, alpha: 1)  // #045a38
   }
 
   struct Size {
@@ -26,6 +33,7 @@ struct Style {
     static let p64: CGFloat = 64
   }
 
+  // TODO: Update this based on style guide from UX/UI person
   enum Typography {
     // TODO: Add more styles as a style guide is made
     case header
@@ -49,7 +57,7 @@ extension UILabel {
   convenience init(_ typography: Style.Typography) {
     self.init()
     font = typography.font
-    textColor = Style.Colors.basic
+    textColor = Style.Colors.black
     numberOfLines = 0
   }
 }
@@ -58,7 +66,7 @@ extension UITextField {
   convenience init(_ typography: Style.Typography) {
     self.init()
     font = typography.font
-    textColor = Style.Colors.basic
+    textColor = Style.Colors.black
   }
 }
 
