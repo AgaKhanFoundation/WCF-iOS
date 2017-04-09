2a1626426e583b737d38ec1701fb873ea3d14da7
Saleem Abdulrasool <compnerd@compnerd.org>
2017-04-09 10:59:04 -0700

    ProfileView: localise ranges
    Localise the range strings, provide a preference for the default range
    selection (first use behaviour), and fix an off-by-one error in the
    range picker.

--- WalkingChallenge/Preferences.swift
+++ WalkingChallenge/Preferences.swift
@@ -7,9 +7,11 @@ enum Pedometer {
 class Preferences {
   static var instance = Preferences()
   var source: Pedometer = .None
+  var profileStatsRange: Int = 0
 
   init() {
     // TODO(compnerd) deserialise user preferences
     source = .HealthKit
+    profileStatsRange = 0
   }
 }
