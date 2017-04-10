b684ce39b3fda47f510e34e9cbb1c664aea1357f
Sami Suteria <sami.suteria@gmail.com>
2017-04-09 18:08:55 -0700

    use UserDefaults for storing preferences

--- WalkingChallenge/Preferences.swift
+++ WalkingChallenge/Preferences.swift
@@ -1,17 +1,31 @@
 
-enum Pedometer {
-  case None
-  case HealthKit
-}
+import Foundation
 
-class Preferences {
-  static var instance = Preferences()
-  var source: Pedometer = .None
-  var profileStatsRange: Int = 0
-
-  init() {
-    // TODO(compnerd) deserialise user preferences
-    source = .HealthKit
-    profileStatsRange = 0
+struct UserInfo {
+  private static let defaults = UserDefaults.standard
+  
+  // Add new keys to store to UserDefaults here
+  private static let pedometerKey = "UserInfo.Keys.Pedometer"
+  private static let profileStatsRangeKey = "UserInfo.Keys.ProfileStatsRange"
+  
+  enum Pedometer: String {
+    case healthKit = "UserInfo.Pedometer.HealthKit"
+    // case fitbit
+  }
+  
+  static var pedometerSource: Pedometer? {
+    get {
+      guard let pedometerRaw = defaults.string(forKey: pedometerKey) else { return nil }
+      return Pedometer(rawValue: pedometerRaw)
+    }
+    set {
+      guard let newValue = newValue else { return }
+      defaults.set(newValue.rawValue, forKey: pedometerKey)
+    }
+  }
+  
+  static var profileStatsRange: Int {
+    get { return defaults.integer(forKey: profileStatsRangeKey) } // 0 returned by default if none set
+    set { defaults.set(newValue, forKey: profileStatsRangeKey) }
   }
 }
