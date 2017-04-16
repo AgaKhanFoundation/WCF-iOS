c71ec50f549658f0b9e975a7f3abb5f040c29d5b
Saleem Abdulrasool <compnerd@compnerd.org>
2017-04-16 14:16:00 -0700

    LeaderboardView: add the leaderboard to the view
    Add a leaderboard instance to the view.  There is currently no fake data
    to present.

--- WalkingChallenge/LeaderboardViewController.swift
+++ WalkingChallenge/LeaderboardViewController.swift
@@ -1,7 +1,22 @@
 
 import SnapKit
 
+class TeamLeaderboardDataSource: LeaderBoardDataSource {
+  var leaders: Array<LeaderBoardEntry> = []
+
+  func reloadData() {
+    // TODO(compnerd) fetch information from the backend
+  }
+}
+
 class LeaderboardViewController: UIViewController {
+  internal let dataSource: TeamLeaderboardDataSource = TeamLeaderboardDataSource()
+  internal let leaderboard: LeaderBoard = LeaderBoard()
+
+  override func viewWillAppear(_ animated: Bool) {
+    dataSource.reloadData()
+  }
+
   override func viewDidLoad() {
     super.viewDidLoad()
     configureView()
@@ -10,6 +25,11 @@ class LeaderboardViewController: UIViewController {
   private func configureView() {
     view.backgroundColor = Style.Colors.white
     title = Strings.NavBarTitles.leaderboard
+
+    view.addSubview(leaderboard)
+    leaderboard.snp.makeConstraints { (ConstraintMaker) in
+      ConstraintMaker.edges.equalToSuperview().inset(Style.Padding.p12)
+    }
   }
 }
 
