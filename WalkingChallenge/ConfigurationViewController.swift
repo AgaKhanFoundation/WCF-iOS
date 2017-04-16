6f7f0e32f79d2665ef72c6910c889ccbad312eb8
Saleem Abdulrasool <compnerd@compnerd.org>
2017-04-15 18:11:27 -0700

    Team: remove border on team leaderboard

---
+++ WalkingChallenge/ConfigurationViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ConfigurationViewController.swift
+//  WalkingChallenge
+//
+//  Created by Saleem Abdulrasool on 4/15/17.
+//  Copyright © 2017 AKDN. All rights reserved.
+//
+
+import Foundation
