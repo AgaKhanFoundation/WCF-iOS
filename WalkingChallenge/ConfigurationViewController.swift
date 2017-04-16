6c2b3c794507f4712226d6fb167bc991f36b7758
Saleem Abdulrasool <compnerd@compnerd.org>
2017-04-15 18:14:06 -0700

    Configuration: add a configuration view
    Add a configuration view, move logout button.

--- WalkingChallenge/ConfigurationViewController.swift
+++ WalkingChallenge/ConfigurationViewController.swift
@@ -1,9 +1,58 @@
-//
-//  ConfigurationViewController.swift
-//  WalkingChallenge
-//
-//  Created by Saleem Abdulrasool on 4/15/17.
-//  Copyright © 2017 AKDN. All rights reserved.
-//
 
-import Foundation
+import SnapKit
+import FBSDKLoginKit
+
+class ConfigurationViewController: UIViewController {
+  let logoutButton = FBSDKLoginButton()
+  let deviceLabel = UILabel(.header)
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    configureNavigation()
+    configureView()
+  }
+
+  private func configureNavigation() {
+    navigationItem.leftBarButtonItem =
+        UIBarButtonItem(barButtonSystemItem: .done, target: self,
+                        action: #selector(dismissView))
+  }
+
+  private func configureView() {
+    view.backgroundColor = Style.Colors.white
+    title = Strings.NavBarTitles.configuration
+
+    view.addSubview(deviceLabel)
+    deviceLabel.text = Strings.Configuration.device
+    deviceLabel.snp.makeConstraints { (ConstraintMaker) in
+      ConstraintMaker.top.equalToSuperview().inset(Style.Padding.p12)
+      ConstraintMaker.left.right.equalToSuperview().inset(Style.Padding.p12)
+    }
+
+    view.addSubview(logoutButton)
+    logoutButton.delegate = self
+    logoutButton.snp.makeConstraints { (ConstraintMaker) in
+      ConstraintMaker.centerX.equalToSuperview()
+      ConstraintMaker.bottom.equalTo(bottomLayoutGuide.snp.top)
+          .offset(-Style.Padding.p12)
+    }
+  }
+
+  func dismissView() {
+    presentingViewController?.dismiss(animated: true, completion: nil)
+  }
+}
+
+extension ConfigurationViewController: FBSDKLoginButtonDelegate {
+  func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+    AppController.shared.logout()
+  }
+
+  func loginButton(_ loginButton: FBSDKLoginButton!,
+                   didCompleteWith result: FBSDKLoginManagerLoginResult!,
+                   error: Error!) {
+    // Left blank because the user should be logged in when reaching this point
+  }
+}
+
