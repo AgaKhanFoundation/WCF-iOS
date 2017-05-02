6315ead952696fa631d3e8d8566ee00958888cee
kforkarim <kforkarim@gmail.com>
2017-05-01 23:32:11 -0500

    Added logout button to tableView Footer.

--- WalkingChallenge/SettingsViewController.swift
+++ WalkingChallenge/SettingsViewController.swift
@@ -1,10 +1,31 @@
-//
-//  ConfigurationTableViewController.swift
-//  WalkingChallenge
-//
-//  Created by Karim Abdul on 4/30/17.
-//  Copyright © 2017 AKDN. All rights reserved.
-//
+/**
+ * Copyright © 2017 Aga Khan Foundation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **/
 
 import Foundation
 import FacebookLogin
@@ -106,6 +127,7 @@ class SettingsSectionDataSource {
 }
 
 fileprivate class SettingsDataSource: NSObject, UITableViewDataSource, UITableViewDelegate {
+
   internal var dataSource: SettingsSectionDataSource?
 
   override init() {
@@ -152,7 +174,6 @@ fileprivate class SettingsDataSource: NSObject, UITableViewDataSource, UITableVi
     cell.accessoryType = UITableViewCellAccessoryType.disclosureIndicator
     return cell
   }
-
   func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
     if section == 0 {
       let headerTitle = dataSource?.fitBitInfo
@@ -164,9 +185,10 @@ fileprivate class SettingsDataSource: NSObject, UITableViewDataSource, UITableVi
   }
 }
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, LoginButtonDelegate {
   fileprivate var configDataSource = SettingsDataSource()
   private var configurationTableView: UITableView = UITableView(frame: CGRect.zero, style:UITableViewStyle.grouped)
+  internal let btnLogout: LoginButton = LoginButton(readPermissions: [])
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -180,6 +202,7 @@ class SettingsViewController: UIViewController {
 
   private func configureSettingsTableView() {
     configurationTableView.dataSource = configDataSource
+    configurationTableView.delegate = configDataSource
     configurationTableView.estimatedRowHeight = 50 //This is an arbitrary number
     configurationTableView.rowHeight = UITableViewAutomaticDimension
     configurationTableView.allowsSelection = true
@@ -191,5 +214,20 @@ class SettingsViewController: UIViewController {
     configurationTableView.snp.makeConstraints { (make) in
       make.edges.equalToSuperview()
     }
+    configurationTableView.tableFooterView = UIView(frame:CGRect(x: 0, y: 0, width: configurationTableView.bounds.size.width, height:50))
+    configurationTableView.tableFooterView?.addSubview(btnLogout)
+    btnLogout.delegate = self
+    btnLogout.snp.makeConstraints { (make) in
+      make.left.equalToSuperview().offset(Style.Padding.p8)
+      make.right.equalToSuperview().offset(-Style.Padding.p8)
+    }
+  }
+  func loginButtonDidLogOut(_ loginButton: LoginButton) {
+    AppController.shared.logout()
+  }
+
+  func loginButtonDidCompleteLogin(_ loginButton: LoginButton,
+                                   result: LoginResult) {
+    // Left blank because the user should be logged in when reaching this point
   }
 }
