05f7e7e83b671e972130f933ae71c72ec26303ae
Saleem Abdulrasool <compnerd@compnerd.org>
2019-02-25 22:40:43 -0800

    WalkingChallenge: delete old UI/UX
    We can reference the code from the source history if we need to, but
    remove the old design to allow us to replace the style guide with the
    refreshed style guide.

--- WalkingChallenge/Support/Async.swift
+++ WalkingChallenge/Support/Async.swift
@@ -36,14 +36,16 @@ typealias Seconds = Double
 
 extension Seconds {
   var timeAfterNow: DispatchTime {
-    return DispatchTime.now() + Double(Int64(self * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
+    return DispatchTime.now() +
+           Double(Int64(self * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
   }
 }
 
-func onMain(delay: Seconds = 0, _ block: @escaping GenericBlock) {
+func onMain(delay: Seconds = 0, _ block: @escaping () -> Void) {
   DispatchQueue.main.asyncAfter(deadline: delay.timeAfterNow, execute: block)
 }
 
-func onBackground(delay: Seconds = 0, _ block: @escaping GenericBlock) {
-  DispatchQueue.global(qos: .background).asyncAfter(deadline: delay.timeAfterNow, execute: block)
+func onBackground(delay: Seconds = 0, _ block: @escaping () -> Void) {
+  DispatchQueue.global(qos: .background)
+               .asyncAfter(deadline: delay.timeAfterNow, execute: block)
 }
