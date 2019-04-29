cc6bb3e4d581951002f7d180e038d847a11c8ae7
Saleem Abdulrasool <compnerd@compnerd.org>
2019-04-28 21:59:54 -0700

    Onboarding: initial skeleton

--- WalkingChallenge/Onboarding/Onboarding.swift
+++ WalkingChallenge/Onboarding/Onboarding.swift
@@ -30,62 +30,54 @@
 import SnapKit
 import UIKit
 
-class WelcomeViewController : UIViewController {                                // swiftlint:disable:this colon
-  private let lblTitle: UILabel =
-      UILabel(typography: .headerTitle)
-  private let imgImage: UIImageView =
-      UIImageView(image: UIImage(imageLiteralResourceName: Assets.LoginPeople))
-  private let lblThanks: UILabel =
-      UILabel(typography: .bodyRegular, color: Style.Colors.green)
-  private let btnContinue: UIButton =
-      UIButton(type: .roundedRect)
-  private let btnSkip: UIButton =
-      UIButton(type: .system)
+class Onboarding : UIPageViewController {                                       // swiftlint:disable:this colon
+  private var pages: [UIViewController] = [
+  ]
+  private var control: UIPageControl = UIPageControl(frame: .zero)
+  private var btnContinue: UIButton = UIButton(type: .roundedRect)
+  private var btnSkip: UIButton = UIButton(type: .system)
+
+  init() {
+    super.init(transitionStyle: .scroll, navigationOrientation: .horizontal,
+               options: nil)
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
 
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    setViewControllers([pages.first!], direction: .forward, animated: true,
+                       completion: nil)
+
     layout()
   }
 
-  private func layout() {                                                       // swiftlint:disable:this function_body_length line_length
+  private func layout() {
     self.view.backgroundColor = .white
 
-    self.view.addSubviews([lblTitle, imgImage, lblThanks, btnContinue, btnSkip])
+    self.view.addSubviews([control, btnContinue, btnSkip])
 
-    lblTitle.text = Strings.Welcome.title
-    lblTitle.textColor = Style.Colors.green
-    lblTitle.snp.makeConstraints { (make) in
-      make.top.equalToSuperview().offset(Style.Size.s64)
-      make.left.equalToSuperview().offset(Style.Size.s24)
-      make.right.equalToSuperview().inset(Style.Size.s24)
-    }
-
-    imgImage.snp.makeConstraints { (make) in
-      make.left.equalToSuperview().offset(Style.Size.s16)
-      make.right.equalToSuperview().inset(Style.Size.s16)
-      make.centerY.equalToSuperview()
-
-      // TODO(compnerd) figure out how to make this scale instead
-      make.height.equalTo(221)
-    }
-
-    lblThanks.text = Strings.Welcome.thanks
-    lblThanks.textAlignment = .center
-    lblThanks.snp.makeConstraints { (make) in
+    control.currentPage = 1
+    control.currentPageIndicatorTintColor = Style.Colors.green
+    control.numberOfPages = pages.count
+    control.pageIndicatorTintColor = Style.Colors.grey
+    control.snp.makeConstraints { (make) in
       make.centerX.equalToSuperview()
-      make.top.equalTo(imgImage.snp.bottom).offset(Style.Size.s48)
     }
 
     btnContinue.layer.borderColor = Style.Colors.green.cgColor
     btnContinue.layer.borderWidth = 1
     btnContinue.layer.cornerRadius = 4
-    btnContinue.setTitle(Strings.Welcome.continue, for: .normal)
+    btnContinue.setTitle(Strings.Onboarding.continue, for: .normal)
     btnContinue.setTitleColor(Style.Colors.green, for: .normal)
-    btnContinue.titleLabel?.font = Style.Typography.subtitleBold.font
     btnContinue.snp.makeConstraints { (make) in
+      make.top.equalTo(control.snp.bottom).offset(Style.Size.s24)
+      make.height.equalTo(Style.Size.s40)
       make.left.equalToSuperview().offset(Style.Size.s32)
       make.right.equalToSuperview().inset(Style.Size.s32)
-      make.height.equalTo(Style.Size.s40)
     }
     btnContinue.addTarget(self, action: #selector(`continue`),
                           for: .touchUpInside)
@@ -107,12 +99,68 @@ class WelcomeViewController : UIViewController {                                
   }
 
   @objc
-  func skip(sender: UIButton!) {
-    AppController.shared.transition(to: .navigation)
+  func `continue`(sender: UIButton!) {
+    guard let viewController = self.viewControllers?.first,
+      let index = self.pages.firstIndex(of: viewController),
+    index < self.pages.count else {
+        return
+    }
+
+    // complete
+    if index == self.pages.count - 1 {
+      AppController.shared.transition(to: .navigation)
+      return
+    }
+
+    // continue
+    setViewControllers([self.pages[index + 1]], direction: .forward,
+                       animated: true, completion: nil)
+    control.currentPage = index + 1
+
+    if index + 1 == pages.count - 1 {
+      self.btnSkip.isHidden = true
+      self.btnContinue.setTitle(Strings.Onboarding.begin, for: .normal)
+      self.btnContinue.setTitleColor(Style.Colors.white, for: .normal)
+      self.btnContinue.backgroundColor = Style.Colors.green
+    }
   }
 
   @objc
-  func `continue`(sender: UIButton!) {
-    AppController.shared.transition(to: .onboarding)
+  func skip(sender: UIButton!) {
+    AppController.shared.transition(to: .navigation)
+  }
+}
+
+extension Onboarding : UIPageViewControllerDataSource {                         // swiftlint:disable:this colon
+  func pageViewController(_ pageViewController: UIPageViewController,
+                          viewControllerBefore viewController: UIViewController)
+      -> UIViewController? {
+    guard let index = pages.firstIndex(of: viewController),
+        index >= 1, index <= pages.count else {
+      return nil
+    }
+    return pages[index - 1]
+  }
+
+  func pageViewController(_ pageViewController: UIPageViewController,
+                          viewControllerAfter viewController: UIViewController)
+      -> UIViewController? {
+    guard let index = pages.firstIndex(of: viewController),
+        index < pages.count - 1 else {
+      return nil
+    }
+    return pages[index + 1]
+  }
+
+  func presentationCount(for pageViewController: UIPageViewController) -> Int {
+    return pages.count
+  }
+
+  func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+    guard let viewController = viewControllers?.first,
+        let index = pages.firstIndex(of: viewController) else {
+      return 0
+    }
+    return index
   }
 }
