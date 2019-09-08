11204d24b701686f36db525e95ada92f9c02e4a7
Sami Suteria <sami.suteria@gmail.com>
2019-09-08 01:32:39 -0700

    add v2 dashboard

---
+++ WalkingChallenge/DashboardV2/Cells/EmptyActivityCardCell.swift
@@ -0,0 +1,100 @@
+/**
+ * Copyright © 2019 Aga Khan Foundation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **/
+
+import UIKit
+import SnapKit
+
+protocol EmptyActivityCellDelegate: class {
+  func emptyActivityCellConnectTapped()
+}
+
+struct EmptyActivityCellContext: CellContext {
+  let cellIdentifier: String = EmptyActivityCell.identifier
+  let title: String
+  let body: String
+  let ctaTitle: String
+}
+
+class EmptyActivityCell: ConfigurableTableViewCell {
+  static let identifier = "EmptyActivityCell"
+  
+  private let cardView = CardViewV2()
+  private let titleLabel = UILabel(typography: .title)
+  private let emptyActivityImageView = UIImageView(image: Assets.onboardingDashboard.image)
+  private let bodyLabel = UILabel(typography: .bodyRegular)
+  private let button = Button()
+  
+  weak var delegate: EmptyActivityCellDelegate?
+  
+  override func commonInit() {
+    super.commonInit()
+    
+    emptyActivityImageView.contentMode = .scaleAspectFit
+    button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+    
+    contentView.addSubview(cardView) {
+      $0.leading.trailing.equalToSuperview().inset(Style.Padding.p24)
+      $0.top.bottom.equalToSuperview().inset(Style.Padding.p12)
+    }
+    
+    cardView.addSubview(titleLabel) {
+      $0.top.equalToSuperview().inset(Style.Padding.p32)
+      $0.leading.trailing.equalToSuperview().inset(Style.Padding.p16)
+    }
+    
+    cardView.addSubview(emptyActivityImageView) {
+      $0.leading.trailing.equalToSuperview().inset(Style.Padding.p64)
+      $0.top.equalTo(titleLabel.snp.bottom).offset(Style.Padding.p8)
+    }
+    
+    cardView.addSubview(bodyLabel) {
+      $0.leading.trailing.equalToSuperview().inset(Style.Padding.p16)
+      $0.top.equalTo(emptyActivityImageView.snp.bottom).offset(Style.Padding.p16)
+    }
+    
+    cardView.addSubview(button) {
+      $0.leading.trailing.equalToSuperview().inset(Style.Padding.p16)
+      $0.top.equalTo(bodyLabel.snp.bottom).offset(Style.Padding.p16)
+      $0.bottom.equalToSuperview().inset(Style.Padding.p32)
+    }
+  }
+  
+  func configure(context: CellContext) {
+    guard let context = context as? EmptyActivityCellContext else { return }
+    
+    titleLabel.text = context.title
+    bodyLabel.text = context.body
+    button.title = context.ctaTitle
+  }
+  
+  @objc
+  func buttonTapped() {
+    delegate?.emptyActivityCellConnectTapped()
+  }
+}
---
+++ WalkingChallenge/DashboardV2/DashboardDataSource.swift
@@ -0,0 +1,88 @@
+/**
+ * Copyright © 2019 Aga Khan Foundation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **/
+
+import UIKit
+
+class DashboardDataSource: TableViewDataSource {
+  var cells = [[CellContext]]()
+  
+  private var name: String?
+  private var image: UIImage?
+  
+  func reload(completion: @escaping GenericBlock) {
+    configureCells()
+    completion()
+    
+    onBackground { [weak self] in
+      Facebook.profileImage(for: "me") { (url) in
+        guard
+          let url = url,
+          let data = try? Data(contentsOf: url),
+          let image = UIImage(data: data)
+        else { return }
+        
+        self?.image = image
+        self?.configureCells()
+        completion()
+      }
+    }
+    
+    onBackground { [weak self] in
+      Facebook.getRealName(for: "me") { (name) in
+        self?.name = name ?? "Could not load"
+        self?.configureCells()
+        completion()
+      }
+    }
+  }
+  
+  func configureCells() {
+    cells = [[
+      ProfileCardCellContext(
+        image: image,
+        name: name ?? "Loading...",
+        teamName: "Team: Global Walkers",
+        eventName: "AKF Spring 2019",
+        eventTimeline: "Current Challenge",
+        disclosureLabel: "View Badges"),
+      EmptyActivityCellContext(
+        title: "Activity",
+        body: "No activity yet. You will need to connect a fitness tracker to track your miles",
+        ctaTitle: "Connect an App or Device"),
+      InfoCellContext(
+        title: "Challenge Progress",
+        body: "No data available. You will be able to see your team's progress once the challenge has started"),
+      DisclosureCellContext(
+        title: "Fundraising Progress",
+        body: "No data available. You will be able to see your progress once the challenge has started.",
+        disclosureTitle: "Invite supporters to pledge",
+        context: nil)
+    ]]
+  }
+}
