6552234623f5404e58aa308bee3c69b232466cb5
Sami Suteria <sami.suteria@gmail.com>
2019-09-20 23:28:15 -0700

    add basic test for ci testing

---
+++ steps4impactTests/Steps4ImpactSpec.swift
@@ -0,0 +1,12 @@
+import Quick
+import Nimble
+
+class Steps4ImpactSpec: QuickSpec {
+  override func spec() {
+    describe("Steps4Impact") {
+      it("should run tests") {
+        expect(true).to(beTrue())
+      }
+    }
+  }
+}
