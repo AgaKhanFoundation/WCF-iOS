e5cba4aff416aa5feca4f727ff7e9ffafb611bf5
Saleem Abdulrasool <compnerd@compnerd.org>
2019-09-23 22:16:38 -0700

    challenge: improve the create team layout screen
    This makes the creation completion screen match the mockups better.

--- Steps4Impact/Challenge/CreateTeamSuccessViewController.swift
+++ Steps4Impact/Challenge/CreateTeamSuccessViewController.swift
@@ -32,17 +32,41 @@ import SnapKit
 
 class CreateTeamSuccessViewController: ViewController {
   private let checkmarkImageView = UIImageView(image: Assets.checkmark.image)
-  private let titleLabel = UILabel(typography: .title)
+  private let titleLabel = UILabel(typography: .bodyRegular)
+  private let lblNextSteps: UILabel = UILabel(typography: .bodyRegular)
   private let inviteButton = Button(style: .primary)
+  private let btnContinue: Button = Button(style: .secondary)
+
+  private let event: Event?
+
+  public init(for event: Event?) {
+    self.event = event
+    super.init(nibName: nil, bundle: nil)
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
 
   override func configureView() {
     super.configureView()
+
     title = Strings.Challenge.CreateTeam.title
+
     checkmarkImageView.contentMode = .scaleAspectFit
+
     titleLabel.text = Strings.Challenge.CreateTeam.successTitle
     titleLabel.textAlignment = .center
+
+    lblNextSteps.text = "Next, invite friends to join. You have \((event?.teamLimit ?? 1) - 1) spots remaining on your team."
+    lblNextSteps.textAlignment = .center
+
     inviteButton.title = "Invite Friends to Join"
     inviteButton.addTarget(self, action: #selector(inviteButtonTapped), for: .touchUpInside)
+
+    btnContinue.title = Strings.Challenge.CreateTeam.continue
+    btnContinue.addTarget(self, action: #selector(continueTapped), for: .touchUpInside)
+
     navigationItem.leftBarButtonItem = UIBarButtonItem(
       image: Assets.close.image,
       style: .plain,
@@ -60,11 +84,22 @@ class CreateTeamSuccessViewController: ViewController {
       $0.top.equalTo(checkmarkImageView.snp.bottom).offset(Style.Padding.p32)
     }
 
+    view.addSubview(lblNextSteps) { (make) in
+      make.leading.trailing.equalToSuperview().inset(Style.Padding.p32)
+      make.top.equalTo(titleLabel.snp.bottom).offset(Style.Padding.p32)
+    }
+
     // TODO(samisuteria) add image upload and visibility toggle
 
-    view.addSubview(inviteButton) {
+    let stack: UIStackView = UIStackView()
+    stack.axis = .vertical
+    stack.spacing = Style.Padding.p8
+    stack.distribution = .fillEqually
+    stack.addArrangedSubviews(inviteButton, btnContinue)
+
+    view.addSubview(stack) {
       $0.leading.trailing.equalToSuperview().inset(Style.Padding.p32)
-      $0.top.equalTo(titleLabel.snp.bottom).offset(Style.Padding.p64)
+      $0.top.equalTo(lblNextSteps.snp.bottom).offset(Style.Padding.p24)
     }
   }
 
@@ -79,5 +114,10 @@ class CreateTeamSuccessViewController: ViewController {
       viewController: self,
       shareButton: inviteButton,
       string: Strings.Share.item)
+  }
+
+  @objc
+  private func continueTapped() {
+    self.dismiss(animated: true)
   }
 }
