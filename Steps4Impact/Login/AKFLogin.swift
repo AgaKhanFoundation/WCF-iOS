28a6be38384ac7b4829c42d706a672eb0f243773
Saleem Abdulrasool <compnerd@compnerd.org>
2019-10-01 10:01:04 -0700

    Login: pass along FBID to AKF signup form

--- Steps4Impact/Login/AKFLogin.swift
+++ Steps4Impact/Login/AKFLogin.swift
@@ -56,7 +56,7 @@ class AKFLoginViewController: ViewController {
       make.width.height.equalToSuperview()
     }
     webview.configuration.userContentController.add(self, name: AKFLoginViewController.LoginCompletedHandlerKey)
-    webview.load(URLRequest(url: URL(string: "https://www.akfusa.org/steps4impact/")!))
+    webview.load(URLRequest(url: URL(string: "https://www.akfusa.org/steps4impact/?fbid=\(Facebook.id)")!))
   }
 }
 
