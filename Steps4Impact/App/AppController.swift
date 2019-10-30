2ecdae8584151551de6eaea5a2a300219c843c6a
Faisal Ali <faisal.ali@outlook.com>
2019-10-29 21:50:32 -0700

    PR changes
    (cherry picked from commit dee16b4deda755a75f1ae4400e0f7c511a37dc6c)

--- Steps4Impact/App/AppController.swift
+++ Steps4Impact/App/AppController.swift
@@ -40,6 +40,8 @@ import FirebaseMessaging
 class AppController {
   static let shared = AppController()
 
+  private init() { }
+
   var window: UIWindow?
   var navigation: UITabBarController = Navigation()
 
