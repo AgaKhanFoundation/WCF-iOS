b01d8048467b61e23771a9a82f5247568e66f7f9
Sami Suteria <sami.suteria@gmail.com>
2020-06-27 22:39:08 -0500

    update cache to reference firebase instead of facebook

--- Steps4Impact/App/Cache.swift
+++ Steps4Impact/App/Cache.swift
@@ -34,36 +34,51 @@ import RxCocoa
 class Cache {
   static let shared = Cache()
 
-  typealias FacebookID = String
-  typealias FacebookName = String
-  typealias FacebookProfileImageURL = URL
+  typealias FirebaseID = String
+  typealias SocialDisplayName = String
+  typealias SocialProfileImageURL = URL
   typealias PersonalProgress = Int
   typealias PersonalCommitment = Int
-
-  let facebookNamesRelay = BehaviorRelay<[FacebookID: FacebookName]>(value: [:])
-  private var facebookNames = [FacebookID: FacebookName]()
-  func update(fbid: FacebookID, name: FacebookName) {
-    facebookNames[fbid] = name
-    facebookNamesRelay.accept(facebookNames)
+  
+  func fetchSocialInfo(fbid: FirebaseID) {
+    AKFCausesService.getParticipantSocial(fbid: fbid) { [weak self] (result) in
+      guard
+        let `self` = self,
+        let response = result.response?.dictionaryValue,
+        let displayName = response["displayName"]?.stringValue,
+        let photoURLRaw = response["photoURL"]?.stringValue,
+        let photoURL = URL(string: photoURLRaw)
+      else { return }
+      
+      self.update(fbid: fbid, name: displayName)
+      self.update(fbid: fbid, url: photoURL)
+    }
+  }
+  
+  let socialDisplayNamesRelay = BehaviorRelay<[FirebaseID: SocialDisplayName]>(value: [:])
+  private var socialDisplayNames = [FirebaseID: SocialDisplayName]()
+  func update(fbid: FirebaseID, name: SocialDisplayName) {
+    socialDisplayNames[fbid] = name
+    socialDisplayNamesRelay.accept(socialDisplayNames)
   }
 
-  let facebookProfileImageURLsRelay = BehaviorRelay<[FacebookID: FacebookProfileImageURL]>(value: [:])
-  private var facebookProfileImageURLs = [FacebookID: FacebookProfileImageURL]()
-  func update(fbid: FacebookID, url: FacebookProfileImageURL) {
-    facebookProfileImageURLs[fbid] = url
-    facebookProfileImageURLsRelay.accept(facebookProfileImageURLs)
+  let socialProfileImageURLsRelay = BehaviorRelay<[FirebaseID: SocialProfileImageURL]>(value: [:])
+  private var socialProfileImageURLs = [FirebaseID: SocialProfileImageURL]()
+  func update(fbid: FirebaseID, url: SocialProfileImageURL) {
+    socialProfileImageURLs[fbid] = url
+    socialProfileImageURLsRelay.accept(socialProfileImageURLs)
   }
 
-  let personalProgressRelay = BehaviorRelay<[FacebookID: PersonalProgress]>(value: [:])
-  private var personalProgresses = [FacebookID: PersonalProgress]()
-  func update(fbid: FacebookID, progress: PersonalProgress) {
+  let personalProgressRelay = BehaviorRelay<[FirebaseID: PersonalProgress]>(value: [:])
+  private var personalProgresses = [FirebaseID: PersonalProgress]()
+  func update(fbid: FirebaseID, progress: PersonalProgress) {
     personalProgresses[fbid] = progress
     personalProgressRelay.accept(personalProgresses)
   }
 
-  let personalCommitmentRelay = BehaviorRelay<[FacebookID: PersonalCommitment]>(value: [:])
-  private var personalCommitments = [FacebookID: PersonalCommitment]()
-  func update(fbid: FacebookID, commitment: PersonalCommitment) {
+  let personalCommitmentRelay = BehaviorRelay<[FirebaseID: PersonalCommitment]>(value: [:])
+  private var personalCommitments = [FirebaseID: PersonalCommitment]()
+  func update(fbid: FirebaseID, commitment: PersonalCommitment) {
     personalCommitments[fbid] = commitment
     personalCommitmentRelay.accept(personalCommitments)
   }
